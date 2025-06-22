1d6e1823d9edc7f7c1c4e4964a8ca964117dcdd2
Piter Pangaribuan <204148314+OmJarot@users.noreply.github.com>
2025-06-22 21:54:46 +0700

    address update

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddressCreateRequest;
+use App\Http\Requests\AddressUpdateRequest;
 use App\Http\Resources\AddressResource;
 use App\Models\Address;
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -15,16 +17,8 @@ class AddressController extends Controller
 {
     function create(int $idContact, AddressCreateRequest $request): JsonResponse {
         $user = Auth::user();
-        $contact = Contact::query()->where("user_id", "=", $user->id)->where("id", "=", $idContact)->first();
-        if (!isset($contact)){
-            throw new HttpResponseException(response()->json([
-                "errors" => [
-                    "message" => [
-                        "Not found"
-                    ]
-                ]
-            ])->setStatusCode(404));
-        }
+
+        $contact = $this->getContact($user, $idContact);
 
         $data = $request->validated();
         $address = new Address($data);
@@ -36,6 +30,27 @@ class AddressController extends Controller
 
     function get(int $idContact, int $idAddress) {
         $user = Auth::user();
+
+        $contact = $this->getContact($user, $idContact);
+        $address = $this->getAddress($contact, $idAddress);
+
+        return new AddressResource($address);
+    }
+
+    function update(int $idContact, int $idAddress, AddressUpdateRequest $request): AddressResource {
+        $user = Auth::user();
+
+        $contact = $this->getContact($user, $idContact);
+        $address = $this->getAddress($contact, $idAddress);
+
+        $data = $request->validated();
+        $address->fill($data);
+        $address->save();
+
+        return new AddressResource($address);
+    }
+
+    private function getContact(User $user, int $idContact): Contact {
         $contact = Contact::query()->where("user_id", "=", $user->id)->where("id", "=", $idContact)->first();
         if (!isset($contact)){
             throw new HttpResponseException(response()->json([
@@ -46,7 +61,10 @@ class AddressController extends Controller
                 ]
             ])->setStatusCode(404));
         }
+        return $contact;
+    }
 
+    private function getAddress(Contact $contact, int $idAddress): Address {
         $address = Address::query()->where("contact_id", $contact->id)->where("id", $idAddress)->first();
         if (!isset($address)){
             throw new HttpResponseException(response()->json([
@@ -57,6 +75,6 @@ class AddressController extends Controller
                 ]
             ])->setStatusCode(404));
         }
-        return new AddressResource($address);
+        return $address;
     }
 }
