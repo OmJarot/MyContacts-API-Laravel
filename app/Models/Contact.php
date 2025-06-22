44dfe4bab12ab18edc87871126a3942287a5106a
Piter Pangaribuan <204148314+OmJarot@users.noreply.github.com>
2025-06-22 15:48:47 +0700

    contact model

---
+++ app/Models/Contact.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Contact extends Model
+{
+    protected $table = "contacts";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    function user(): BelongsTo {
+        return $this->belongsTo(User::class, "user_id", "id");
+    }
+}
