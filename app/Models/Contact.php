e9a105a959a22c279cd13431a1fd35040ee2bb3d
Piter Pangaribuan <204148314+OmJarot@users.noreply.github.com>
2025-06-22 15:54:13 +0700

    address model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
@@ -15,5 +16,9 @@ class Contact extends Model
 
     function user(): BelongsTo {
         return $this->belongsTo(User::class, "user_id", "id");
+    }
+
+    function addresses(): HasMany {
+        return $this->hasMany(Address::class, "contact_id", "id");
     }
 }
